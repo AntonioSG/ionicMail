ae7637ae16178af41f0b0288b1a0cf52bca8e992
rafaelMunoz77 <rafael.munoz77@gmail.com>
2020-11-05 23:04:37 +0100

    Módulo 7 Video 6 - Página de login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from '../../providers/usuario.service';
+import { NavController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { AutenticadorJwtService } from '../../providers/autenticador-jwt.service'; 
+import { ComunicacionDeAlertasService } from '../../providers/comunicacion-de-alertas.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +16,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  waiting: boolean = false;
+  loginForm: FormGroup; // Permite tener un objeto linkado a los campos del formulario de autenticación
+
+
+  constructor(private usuarioService: UsuarioService, private router: Router,
+    private autenticadorJwtService: AutenticadorJwtService,
+    private comunicacionAlertas: ComunicacionDeAlertasService,
+    private navControler: NavController) { }
 
   ngOnInit() {
+        // Inicializo el objeto FormGroup, es la base para usar formularios reactivos, en los que la validación
+    // y el control son muy fáciles de realizar.
+    this.loginForm = new FormGroup({
+      usuario: new FormControl ('rafa', [Validators.required, Validators.minLength(4)]),
+      password: new FormControl ('1234', [Validators.required])
+    });
+  }
+
+
+  autenticaUsuario () {
+    this.waiting = true;
+    this.usuarioService.autenticaUsuario(this.loginForm.controls.usuario.value, 
+      this.loginForm.controls.password.value).subscribe(data => {
+        console.log(data);
+        this.waiting = false;  
+        if (data.jwt != undefined) {
+          this.autenticadorJwtService.almacenaJWT(data.jwt); // Almaceno un nuevo JWT
+          this.navControler.navigateForward('/listado-mensajes');
+        } 
+        else {
+          this.comunicacionAlertas.mostrarAlerta("Usuario y/o password incorrectos");
+        }
+      });
   }
 
 }
